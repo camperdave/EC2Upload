b065d39083e20cb0c27bf15057959380461d6400
Dave Lee <camperdave614@gmail.com>
2012-09-10 12:10:59 -0500

    Some fixes made in class to work on CEC

--- upload.bat
+++ upload.bat
@@ -2,9 +2,10 @@
 REM DO NOT MODIFY ABOVE THIS LINE!!!!
 
 SET UNAME="dave"
-SET EC2URL="ec2-184-72-138-38.compute-1.amazonaws.com"
-SET KEYPATH="C:\Users\Dave\Keys\EC2\Jet.ppk"
+SET EC2URL="ec2-50-16-26-96.compute-1.amazonaws.com"
+SET KEYPATH="H:\keys\Jet.ppk"
 SET BASEFOLDER="/var/www/html"
+SET PUTTYFOLDER="H:\putty"
 
 REM DO NOT MODIFY BELOW THIS LINE!!!!!!
 
@@ -16,11 +17,11 @@ REM		3 		$project_base_name
 echo NOW UPLOADING: %1
 
 IF %3%=="" (
-		plink.exe -ssh -i "%KEYPATH%" "%UNAME%@%EC2URL%" "mkdir -p /var/www/html/"
+		%PUTTYFOLDER%\plink.exe -ssh -i "%KEYPATH%" "%UNAME%@%EC2URL%" "mkdir -p /var/www/html/"
 
-		pscp.exe -i "%KEYPATH%" "%2" "%UNAME%@%EC2URL%:/var/www/html/%1"
+		%PUTTYFOLDER%\pscp.exe -i "%KEYPATH%" "%2" "%UNAME%@%EC2URL%:/var/www/html/%1"
 	) ELSE (
-		plink.exe -ssh -i "%KEYPATH%" "%UNAME%@%EC2URL%" "mkdir -p /var/www/html/%3/"
+		%PUTTYFOLDER%\plink.exe -ssh -i "%KEYPATH%" "%UNAME%@%EC2URL%" "mkdir -p /var/www/html/%3/"
 
-		pscp.exe -i "%KEYPATH%" "%2" "%UNAME%@%EC2URL%:%BASEFOLDER%/%3/%1"
+		%PUTTYFOLDER%\pscp.exe -i "%KEYPATH%" "%2" "%UNAME%@%EC2URL%:%BASEFOLDER%/%3/%1"
 	)
